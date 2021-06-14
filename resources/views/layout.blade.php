a4d83f3e19fd8d431b458e685215c46cb9010eb8
Dawid Wolosowicz <d@1823.pl>
2021-06-14 19:25:09 +0200

    App: Add laravel/breeze and laravel/telescope

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>{{ config('app.name') }}</title>
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    @yield('body')
+</body>
+</html>
